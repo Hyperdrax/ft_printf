0f0a041e989b24fd910f72f018c187d83c7014fd
flhensel <flhensel@student.42heilbronn.de>
2025-10-06 15:20:13 +0200

    norm

--- ft_nbr.c
+++ ft_nbr.c
@@ -6,7 +6,7 @@
 /*   By: flhensel <flhensel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 09:47:59 by flhensel          #+#    #+#             */
-/*   Updated: 2025/09/04 09:48:39 by flhensel         ###   ########.fr       */
+/*   Updated: 2025/10/06 15:17:05 by flhensel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int	*write_number(int n)
 	free(nbr);
 	return (length);
 }
-
 
 int	*write_unsigned(unsigned int n)
 {
